aa182b037caba5b32be5f18412aaa867dc864041
Elie-MK <elie.mk25@gmail.com>
2024-10-05 18:16:40 +0100

    add application files

---
+++ src/main/java/com/rakbank/users/UsersApplication.java
@@ -0,0 +1,17 @@
+package com.rakbank.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+public class UsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApplication.class, args);
+	}
+
+}
